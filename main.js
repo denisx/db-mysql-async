c71549acbf538651d35d452517ec7b8188e3c39d
denisx <github.com@denisx.ru>
2017-08-29 11:33:36 +0300

    v 0.0.1

---
+++ main.js
@@ -0,0 +1,47 @@
+class DBMysqlAsync {
+	constructor(opts) {
+		const mysql = require('mysql')
+		this.pool = mysql.createPool(opts)
+	}
+
+	escape(str) {
+		return this.pool.escape(str)
+	}
+
+	async getConnection() {
+		return new Promise((resolve, reject) => {
+			this.pool.getConnection((err, connection) => {
+				if (err) {
+					reject(err)
+				} else {
+					resolve(connection)
+				}
+			})
+		})
+	}
+
+	async sql(query) {
+		const connection = await this.getConnection()
+			.catch(err => console.error('Mysql getConnection', err))
+
+		return new Promise((resolve, reject) => {
+			connection.query(query, (err, rows) => {
+				connection.destroy()
+				if (err) {
+					reject(err)
+				} else {
+					resolve(rows)
+				}
+			})
+		})
+	}
+}
+
+module.exports = (opts) => {
+	if (!opts) {
+		console.error('Empty opts')
+		return
+	}
+
+	return new DBMysqlAsync(opts)
+}
