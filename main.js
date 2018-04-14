7a19efcaf73e360f5a8468ed2a1753ed8a94f902
denisx <github.com@denisx.ru>
2018-04-14 12:28:30 +0300

    reconnect opts

--- main.js
+++ main.js
@@ -1,61 +1,118 @@
+const delay = async(time) => {
+	return new Promise((resolve) => {
+		const timer = setTimeout(() => {
+			clearTimeout(timer)
+			resolve()
+		}, time)
+	})
+		.catch(err => {
+			console.error(err)
+		})
+}
+
 class DBMysqlAsync {
-	constructor(opts) {
+	constructor(opts, params = {}) {
 		const mysql = require('mysql')
 		this.pool = mysql.createPool(opts)
+
+		this.mustReconnect = params.reconnect
+		this.stepInc = params.stepInc || 50
+		this.stepSumMax = params.stepSumMax || 3000
 	}
 
 	escape(str) {
 		return this.pool.escape(str)
 	}
 
-	async getConnection() {
-		return new Promise((resolve, reject) => {
+	async getConnection(step = 0, stepSum = 0) {
+		const con = await new Promise((resolve, reject) => {
+			const timer = setTimeout(() => {
+				clearTimeout(timer)
+				reject({code: 'ECONNREFUSED'})
+			}, this.stepSumMax)
+
 			this.pool.getConnection((err, connection) => {
+				clearTimeout(timer)
+
+				// TODO DEV - emulate connection errors
+				// if (Math.random() < 0.3) {
+				// 	connection.release()
+				// 	reject({code: 'ECONNREFUSED'})
+				// }
+
 				if (err) {
 					reject(err)
-				} else {
-					resolve(connection)
-				}
-			})
-		})
-	}
-
-	async sql(query) {
-		const connection = await this.getConnection()
-			.catch(err => {
-				if (!err) {
 					return
 				}
 
-				switch (err) {
-					default:
-						console.error('Mysql getConnection error, err=', err)
-				}
+				resolve(connection)
 			})
+		})
+			.then(res => ({status: 'ok', connection: res}))
+			.catch(err => ({status: 'no', err}))
 
-		if (!connection) {
-			return Promise.reject('no connection')
+		if (con.status === 'ok') {
+			return con.connection
+		}
+
+		if (this.mustReconnect) {
+			return await this.reconnect(con.err, step + 1, stepSum)
+		}
+
+		return Promise.reject(con.err)
+	}
+
+	async reconnect(err = {}, step, stepSum) {
+		if ([
+			'ECONNREFUSED',
+			'PROTOCOL_CONNECTION_LOST'
+		].includes(err.code || '')) {
+			if (stepSum > this.stepSumMax) {
+				return Promise.reject(err)
+			}
+
+			const newDelay = step * this.stepInc
+			stepSum += newDelay
+			await delay(newDelay)
+
+			return this.getConnection(step, stepSum)
+		}
+
+		return Promise.reject(err)
+	}
+
+
+	async sql(query) {
+		const con = await this.getConnection()
+			.then(res => ({status: 'ok', connection: res}))
+			.catch(err => ({status: 'no', err}))
+
+		if (con.status === 'no') {
+			return Promise.reject(con.err)
 		}
 
 		return new Promise((resolve, reject) => {
-			connection.query(query, (err, rows) => {
-				connection.release()
+			con.connection.query(query, (err, res) => {
+				con.connection.release()
 
 				if (err) {
 					reject(err)
-				} else {
-					resolve(rows)
+					return
 				}
+
+				resolve(res)
 			})
 		})
+			.catch(err => {
+				console.error(err)
+			})
 	}
 }
 
-module.exports = (opts) => {
+module.exports = (opts, params) => {
 	if (!opts) {
-		console.error('Empty opts')
 		return
 	}
 
-	return new DBMysqlAsync(opts)
+	return new DBMysqlAsync(opts, params)
 }
